390b8c4e76c5abac848cf26e97b7f199ff98db49
ch04937 <97hernandez.c@gmail.com>
2019-08-09 09:15:14 -0700

    answered self study questions and created registration form

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import FormikRegistrationForm from './component/FormikRegistrationForm'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <FormikRegistrationForm />
+    </div>
+  );
+}
+
+export default App;
